bd4eef2cebd6fa109746883be28c0d069bac029c
Victor Swanberg <victor.swanberg@sublimemedia.com>
2021-04-20 08:47:26 -0700

    Added in a form and form verification, changes the contact component to class component

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
 
@@ -9,7 +10,6 @@ function RenderCampsite({campsite}) {
       <Card>
         <CardImg top src={campsite.image} alt={campsite.name} />
         <CardBody>
-          <CardTitle>{campsite.name}</CardTitle>
           <CardText>{campsite.description}</CardText>
         </CardBody>
       </Card>
@@ -36,13 +36,27 @@ function RenderComments({comments}) {
   return <div />
 }
 
-  function CampsiteInfo(props) {
+function CampsiteInfo(props) {
   if (props.campsite) {
     return (
       <div className="container">
         <div className="row">
+          <div className="col">
+            <Breadcrumb>
+              <BreadcrumbItem>
+                <Link to="/directory">Directory</Link>
+              </BreadcrumbItem>
+              <BreadcrumbItem>
+                <Link active>{props.campsite.name}</Link>
+              </BreadcrumbItem>
+            </Breadcrumb>
+            <h2>{props.campsite.name}</h2>
+            <hr />
+          </div>
+        </div>
+        <div className="row">
           <RenderCampsite campsite={props.campsite} />
-          <RenderComments comments={props.campsite.comments} />
+          <RenderComments comments={props.comments} />
         </div>
       </div>
     );
